0b7c4d38e81f52fd5c659bb6c834b6bb166977ba
Caleb <caleb.falcione@gmail.com>
2019-09-10 23:57:30 -0500

    removes exception handling

--- location-bot/src/index.js
+++ location-bot/src/index.js
@@ -1,6 +1,6 @@
 const { openWebSocket } = require('./websocket')
 const { locationStream, mockData } = require('./location-data')
-const { map, tap, catchError } = require('rxjs/operators')
+const { map, tap } = require('rxjs/operators')
 
 const wsHost = 'ws://34.70.173.43:1880/ws/bus-simulator'
 
@@ -10,16 +10,14 @@ const makeLocationEmitter = (socket) => ({ lat, lng, busID, timestamp }) => {
     payload: { lat, lng, busID },
     timestamp,
   })
-  try {
-    socket.send(message)
-  } catch {/* I know, I know. */}
+  socket.send(message)
   console.log(message)
 }
 
 const main = (socket) => {
   const emitLocation = makeLocationEmitter(socket)
 
-  locationStream(mockData, 250, 20)
+  locationStream(mockData, 1000, 20)
     .pipe(
       map(([lat, lng]) => {
         return {
@@ -30,7 +28,6 @@ const main = (socket) => {
         }
       }),
       tap(emitLocation),
-      catchError((err) => console.error(err))
     )
     .subscribe()
 }
